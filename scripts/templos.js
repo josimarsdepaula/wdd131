4c3e29650d26b929cdd0aef5805184b5359d1e9d
josimarsdepaula <jpaula@byupathway.edu>
2026-07-11 15:20:52 -0300

    S02 - templos - correção Auditoria

--- scripts/templos.js
+++ scripts/templos.js
@@ -3,3 +3,11 @@ document.getElementById("anoatual").textContent = anoAtual;
 
 document.getElementById("ultimaModificacao").textContent =
 "Última Modificação: " + document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav.navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menuButton.classList.toggle("open");
+});
